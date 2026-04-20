f1a56e21de992c279e3e895c94c22e7ff61cff1e
asisworkrn <asisworkrn@gmail.com>
2026-04-20 12:08:52 +0530

    -splash screen added for both ios & android

--- src/navigation/root/RootNavigator.tsx
+++ src/navigation/root/RootNavigator.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "../auth/AuthNavigator";
 import { navigationRef } from "../navigationRef";
 import AppNavigator from "../app/AppNavigator";
 import { useAuthStore } from "@src/state/useAuthStore";
+import BootSplash from "react-native-bootsplash";
 
 
 export default function RootNavigator() {
   const token = useAuthStore((state) => state.token);
   const isLoggedIn = !!token;
 
+  const onReady = useCallback(async () => {
+    setTimeout(()=>{
+  BootSplash.hide();
+    },3000)
+  
+  }, []);
+
   return (
-    <NavigationContainer ref={navigationRef}>
+    <NavigationContainer onReady={onReady} ref={navigationRef}>
       {isLoggedIn ? <AppNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   );
